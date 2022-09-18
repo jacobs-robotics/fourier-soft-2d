4524bd9276d8b9e80f573caa793a7444d5805904
zarbokk <timhansen93@googlemail.com>
2022-09-18 13:28:34 +0200

    added matlab file

--- tools/matlabVisualization/plotAllPossibleSolutionsForOneScan.m
+++ tools/matlabVisualization/plotAllPossibleSolutionsForOneScan.m
@@ -7,7 +7,6 @@ nameOfFolder = "out";
 
 %%
 
-set(groot,'defaultAxesTickLabelInterpreter','latex');  
 
 magnitudeFFTW1 = readmatrix(nameOfFolder+"/magnitudeFFTW1.csv");
 N=sqrt(size(magnitudeFFTW1,1));
@@ -18,14 +17,14 @@ voxelDataUsed1 = readmatrix(nameOfFolder+"/voxelDataFFTW1.csv");
 magnitudeFFTW2 = readmatrix(nameOfFolder+"/magnitudeFFTW2.csv");
 phaseFFTW2 = readmatrix(nameOfFolder+"/phaseFFTW2.csv");
 voxelDataUsed2 = readmatrix(nameOfFolder+"/voxelDataFFTW2.csv");
-%fftwResult = complex(realFFTW,imaginaryFFTW);
-%fftwResult = (reshape(fftwResult,N,N,N));
+
 magnitude1 =zeros(N,N);
 phase1 =zeros(N,N);
 voxelData1 =zeros(N,N);
 magnitude2 =zeros(N,N);
 phase2 =zeros(N,N);
 voxelData2 =zeros(N,N);
+
 for j =1:N
     for k =1:N
         magnitude1(k,j) = magnitudeFFTW1(k*N-N+j);
@@ -37,7 +36,6 @@ for j =1:N
     end
 end
 
-
 magnitude1=fftshift(magnitude1);
 phase1=fftshift(phase1);
 magnitude2=fftshift(magnitude2);
@@ -45,44 +43,35 @@ phase2=fftshift(phase2);
 
 figure(1)
 clf
-% sigma = 70;
-% size2 = size(magnitude1,1);
-% [X, Y] = meshgrid(1:size2, 1:size2);
-% G = 1*exp(-1/(sigma^2)*((Y-size2/2).^2 + (X-size2/2).^2));
-% G = 1-G;
-% imagesc(squeeze(G));
 
 imagesc(squeeze(magnitude1));
 
-%title('Magnitude first PCL Voxel:', 'Interpreter', 'latex')
+title('Magnitude Voxel 1:', 'Interpreter', 'latex')
 axis image
 box on 
-pbaspect([1 1 1])
+
 
 figure(2)
 clf
 imagesc((voxelData1))
 axis image
-%title('First PointCloud as Voxel:', 'Interpreter', 'latex')
-% figure 2
-pbaspect([1 1 1])
+title('Voxel Data 1:', 'Interpreter', 'latex')
+
 
 figure(3)
 clf
 
 imagesc(squeeze(magnitude2));
 
-
-%imshow(magnitude2(:,:,size(magnitude2,1)/2));
-%title('Magnitude second PCL Voxel:', 'Interpreter', 'latex')
+title('Magnitude Voxel 2:', 'Interpreter', 'latex')
 axis image
-pbaspect([1 1 1])
 
 figure(4)
+
 imagesc((voxelData2))
+
 axis image
-%title('Second PointCloud as Voxel:', 'Interpreter', 'latex')
-pbaspect([1 1 1])
+title('Voxel Data 2:', 'Interpreter', 'latex')
 %%
 resampledDataForSphere1 = readmatrix(nameOfFolder+"/resampledVoxel1.csv");
 resampledDataForSphere2 = readmatrix(nameOfFolder+"/resampledVoxel2.csv");
@@ -95,8 +84,7 @@ for j = 1:N
             resampledDataForSphereResult2(j,i) = resampledDataForSphere2((i-1)*N+j);
     end
 end
-
-% currently not that interesting
+% can also be printed as 2D instead of a sphere
 if 1
     figure(5)
     clf
@@ -105,30 +93,28 @@ if 1
     ch = get(gca,'children');
     set(ch,'facecolor','texturemap','cdata',resampledDataForSphereResult2,'edgecolor','none');
     axis equal
-    %title('Second Resampled:', 'Interpreter', 'latex')
-    pbaspect([1 1 1])
+    title('Resampled Sphere 2:', 'Interpreter', 'latex')
     figure(6)
     clf
     sphere(1000);
     ch = get(gca,'children');
     set(ch,'facecolor','texturemap','cdata',resampledDataForSphereResult1,'edgecolor','none');
     axis equal
-    %title('first Resampled:', 'Interpreter', 'latex')
-    pbaspect([1 1 1])
+    title('Resampled Sphere 1:', 'Interpreter', 'latex')
    
 else
     figure(5)
     clf
     imagesc((resampledDataForSphereResult2));
     axis image
-        %title('second Resampled:', 'Interpreter', 'latex')
+    title('Resampled Sphere as 2D 2:', 'Interpreter', 'latex')
     pbaspect([1 1 1])
     figure(6)
     clf
     imagesc((resampledDataForSphereResult1));
     axis image
     pbaspect([1 1 1])
-        %title('first Resampled:', 'Interpreter', 'latex')
+    title('Resampled Sphere as 2D 1:', 'Interpreter', 'latex')
 end
 
 %%
@@ -138,21 +124,13 @@ correlationOfAngles = readmatrix(nameOfFolder+"/resultingCorrelation1D.csv");
 anglesX = linspace(0,2*pi,size(correlationOfAngles,1));
 [psor,lsor] = findpeaks(correlationOfAngles);
 plot(anglesX,correlationOfAngles)
-for i=1:size(psor,1)
-    text(anglesX(lsor(i))+0.15,psor(i)+1,[num2str(round(anglesX(lsor(i)),2)) ], 'Interpreter', 'latex')
-end
+
 grid on
 xlim([-0.2 2*pi+0.2])
-%ylim([min(correlationOfAngles)-10 max(correlationOfAngles)+10])
-%title('Correlation of different Angles', 'Interpreter', 'latex')
+
 box on 
 xlabel("angle in rad", 'Interpreter', 'latex');
 ylabel("Correlation Value", 'Interpreter', 'latex');
-pbaspect([1 1 1])
-%%
-
-
-
 
 %%
 
@@ -162,34 +140,22 @@ numberOfSolutions = dataInformation(1);
 bestSolution = dataInformation(2);
 
 
-
-
-
-% [M,I] = max(correlationMatrixShift2D, [], "all", "linear");
-% [dim1, dim2, dim3] = ind2sub(size(correlationMatrixShift2D),I)
-
-%correlationMatrixShift2D  = squeeze(correlationMatrixShift3D(65,:,:));
 figure(8)
 clf
-
+%correlation
 for i = 1:numberOfSolutions
         subplot(2,2,i)
     correlationMatrixShift1D = readmatrix(nameOfFolder+"/resultingCorrelationShift"+ string(num2str(i-1)) +".csv");
     resultSize = nthroot(length(correlationMatrixShift1D),2);
-    %A = reshape(results,resultSize,resultSize);
     correlationMatrixShift2D = reshape(correlationMatrixShift1D,resultSize,resultSize);
-%     imagesc(correlationMatrixShift2D);
     [Xplot,Yplot]=meshgrid(1:resultSize,1:resultSize);
     surf(Xplot,Yplot,(correlationMatrixShift2D),'edgecolor', 'none');
-    %xlabel("x-axis", 'Interpreter', 'latex');
-    %ylabel("y-axis", 'Interpreter', 'latex');
+
     
-    %title('Correlation Surface of  Best Performing Match', 'Interpreter', 'latex')
     box on 
     title(num2str(i), 'Interpreter', 'latex')
     pbaspect([1 1 1])
 end
-%set(gcf,'units','points','position',[0,0,500,500])
 
 
 
@@ -204,13 +170,11 @@ clf
 
 
 
-
+% show results of registrated voxels, 
 for i = 1:numberOfSolutions
     resultVoxel1TMP= readmatrix(nameOfFolder+"/resultVoxel1"+num2str(i-1)+".csv");
     resultVoxel2TMP= readmatrix(nameOfFolder+"/resultVoxel2"+num2str(i-1)+".csv");
 
-    %fftwResult = complex(realFFTW,imaginaryFFTW);
-    %fftwResult = (reshape(fftwResult,N,N,N));
     voxelResult1 =zeros(N,N);
     voxelResult2 =zeros(N,N);
     for j =1:N
@@ -230,6 +194,7 @@ for i = 1:numberOfSolutions
     title(num2str(i), 'Interpreter', 'latex')
 
     box on
+    pbaspect([1 1 1])
 
 
 end
